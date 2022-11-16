28343356fe1ddf7ffa38794655348d2fcdb3ffa9
Raphael Derosso Pereira <raphaelpereira@gmail.com>
2022-11-16 17:54:33 -0300

    fix: do not double marshal HTTP payload

--- v2/pkg/spike/http-server.go
+++ v2/pkg/spike/http-server.go
@@ -215,7 +215,7 @@ func (h *httpServer) httpHandler(p rids.Pattern, w http.ResponseWriter, r *http.
 	}
 
 	p = p.Clone()
-	call := broker.NewHTTPCall(p, token, string(data), params, r.Form.Encode())
+	call := broker.NewHTTPCall(p, token, data, params, r.Form.Encode())
 	result, rErr := h.opts.Broker.RequestRaw(p.EndpointName(), call.ToJSON())
 	if rErr != nil {
 		w.WriteHeader(http.StatusInternalServerError)
